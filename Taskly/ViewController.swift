d74490aa2ef5a03cb7f0d2de693e8e69f65a6320
adenyx <adenis124@gmail.com>
2026-09-12 00:57:16 +0300

    [#1] Initial project setup

---
+++ Taskly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Taskly
+//
+//  Created by admin on 12.09.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TasklyTests/TasklyTests.swift
@@ -0,0 +1,17 @@
+//
+//  TasklyTests.swift
+//  TasklyTests
+//
+//  Created by admin on 12.09.2026.
+//
+
+import Testing
+@testable import Taskly
+
+struct TasklyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
